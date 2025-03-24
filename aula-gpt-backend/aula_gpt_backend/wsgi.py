3874f3521db44f0433838992da07dac80eb9aec4
Hugo <hugoagreda22@gmail.com>
2025-03-24 13:06:49 +0100

    Configuración de Procfile para el backend y frontend

--- aula-gpt-backend/aula_gpt_backend/wsgi.py
+++ aula-gpt-backend/aula_gpt_backend/wsgi.py
@@ -2,15 +2,15 @@
 WSGI config for aula_gpt_backend project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
+# Establecer la configuración de Django para producción
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aula_gpt_backend.settings')
 
+# Exponer la aplicación WSGI
 application = get_wsgi_application()
