347b271a064f8ca3ca7a309b6e5ea749ce6585e8
Hugo <hugoagreda22@gmail.com>
2025-06-03 15:40:42 +0200

    Preparando backend para despliegue en Render

--- aulagpt-backend/api/urls.py
+++ aulagpt-backend/api/urls.py
@@ -18,7 +18,7 @@ router.register(r'testanswers', TestAnswerViewSet)
 router.register(r'activities', ActivityViewSet)
 
 urlpatterns = [
-    path('api', include(router.urls)),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('', include(router.urls)),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
