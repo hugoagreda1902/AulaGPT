4a8423f1715980141a2774294c76b1aade5ec950
Hugo <hugoagreda22@gmail.com>
2025-06-04 09:50:52 +0200

    Preparando backend para despliegue en Render

--- aulagpt-frontend/src/App.jsx
+++ aulagpt-frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Register from "./components/Register";
+import SubirDocumento from './components/SubirDocumento';
 
 function App() {
   return (
