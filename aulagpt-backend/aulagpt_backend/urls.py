86fea6927450c265eb5def1339121b54ce4a83f0
Hugo <hugoagreda22@gmail.com>
2025-06-04 23:32:40 +0200

    Preparando backend para despliegue en Render

--- aulagpt-backend/aulagpt_backend/urls.py
+++ aulagpt-backend/aulagpt_backend/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.http import JsonResponse
-from rest_framework_simplejwt.views import TokenObtainPairView
 
 def home(request):
     return JsonResponse({"message": "API AulaGPT funcionando"})
@@ -10,6 +9,4 @@ urlpatterns = [
     path('', home),  # Ruta raíz /
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),  # Enlaza las urls de tu app 'api'
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
 ]
-
